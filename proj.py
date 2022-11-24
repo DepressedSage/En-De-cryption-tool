1a04c2a45a4c3615ffa7602db3001a65aa733368
DepressedSage <adit.sharma270@gmail.com>
2022-11-25 00:31:15 +0530

    Added base64 encoder

--- proj.py
+++ proj.py
@@ -1,5 +1,31 @@
 import argparse
+import base64
 from cryptography.fernet import Fernet
+
+def Vig_generateKey(string, key):
+    key = list(key)
+    if len(string) == len(key):
+        return(key)
+    else:
+      for i in range(len(string) -len(key)):
+          key.append(key[i % len(key)])
+          return("" . join(key))
+
+def Vig_encryption(string, key):
+    encrypt_text = []
+    for i in range(len(string)):
+        x = (ord(string[i]) +ord(key[i])) % 26
+        x += ord('A')
+        encrypt_text.append(chr(x))
+    return("" . join(encrypt_text))
+
+def Vig_decryption(encrypt_text, key):
+    orig_text = []
+    for i in range(len(encrypt_text)):
+        x = (ord(encrypt_text[i]) -ord(key[i]) + 26) % 26
+        x += ord('A')
+        orig_text.append(chr(x))
+    return("" . join(orig_text))
 
 key = Fernet.generate_key()
 
@@ -19,11 +45,34 @@ decrypt.add_argument('-file', type=str,required=True)
 decrypt.add_argument('-type', type=str,required=True)
 args = parser.parse_args()
 
+with open(args.file, 'r+') as file:
+    original = file.read()
+tmpkey = Fernet.generate_key()
+tmpkey = str(tmpkey)
+VigKey = Vig_generateKey(original, original[0:5])
 if args.command == 'encrypt':
-    if args.type == '' or 'fernet':
+    if args.type == 'b64':
+        with open(args.file, 'r+') as file:
+            original = file.read()
+            original_bytes = original.encode('ascii')
+            encrypted_bytes = base64.b64encode(original_bytes)
 
+        with open(args.file, 'w+') as encrypted_file:
+            encrypted = encrypted_bytes.decode('ascii')
+            encrypted_file.write(encrypted)
+
+    elif args.type == 'vig':
+        with open(args.file, 'r+') as file:
+            original = file.read()
+
+        encrypted = Vig_encryption(original,VigKey)
+
+        with open(args.file, 'w+') as encrypted_file:
+            encrypted_file.write(encrypted)
+
+    elif args.type == 'fernet':
         with open('filekey.key', 'rb') as filekey:
-            filekey.read()
+            key = filekey.read()
         fernet = Fernet(key)
 
         with open(args.file, 'rb') as file:
@@ -34,10 +83,29 @@ if args.command == 'encrypt':
         with open(args.file, 'wb') as encrypted_file:
             encrypted_file.write(encrypted)
 
-if args.command == 'decrypt':
-    if args.type == '' or 'fernet':
+elif args.command == 'decrypt':
+    if args.type == 'b64':
+        with open(args.file, 'r+') as encrypted_file:
+            encrypted = encrypted_file.read()
+            encrypted_bytes = encrypted.encode('ascii')
+            decrypted_bytes = base64.b64decode(encrypted_bytes)
+
+        with open(args.file, 'w+') as decrypted_file:
+            decrypted = decrypted_bytes.decode('ascii')
+            decrypted_file.write(decrypted)
+
+    elif args.type == 'vig':
+        with open(args.file, 'r+') as encrypted_file:
+            encrypted = encrypted_file.read()
+
+        decrypted = Vig_decryption(encrypted,VigKey)
+
+        with open(args.file, 'w+') as decrypted_file:
+            decrypted_file.write(decrypted)
+
+    elif args.type == '' or 'fernet':
         with open('filekey.key', 'rb') as filekey:
-            filekey.read()
+            key = filekey.read()
         fernet = Fernet(key)
 
         with open(args.file,'rb') as enc_file:
